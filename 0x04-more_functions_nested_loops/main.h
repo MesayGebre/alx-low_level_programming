03fb72fbf9d065ee93ed660847ce6a846f2667f5
MesayGebre <mesaygebre2@gmail.com>
2022-09-15 08:10:33 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,30 @@
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+/*
+
+int _isupper(int c);                                                                                                                                   
+
+int _isdigit(int c);                                                                                                                                   
+
+int mul(int a, int b);                                                                                                                                 
+
+void print_numbers(void);                                                                                                                              
+
+void print_most_numbers(void);                                                                                                                         
+
+void more_numbers(void);                                                                                                                               
+
+void print_line(int n);                                                                                                                                
+
+void print_diagonal(int n);                                                                                                                            
+
+void print_square(int size);                                                                                                                           
+
 void print_triangle(int size);
+#endif /* MAIN.H /*
+^C
