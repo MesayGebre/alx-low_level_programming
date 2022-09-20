ada5372a1d666a01c23c89d01d37226410f83d0f
MesayGebre <mesaygebre2@gmail.com>
2022-09-20 10:19:07 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
-	
+
 	i = 0;
 	d = 0;
 	n = 0;
@@ -24,7 +24,8 @@ int _atoi(char *s)
 
 		if (s[i] == '-')
 			++d;
-		if (s[i] >= '0' && s[i] <= '9')															{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
