86503d1fe952e6901277d3e32d8f75c3e15fedd9
MesayGebre <mesaygebre2@gmail.com>
2022-09-30 21:07:03 -0700

    Write a program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * main - Entry Point
  * @argc: arguments
  * @argv: array pointing to arguments
+ *
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
